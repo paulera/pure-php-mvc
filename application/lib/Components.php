637d06cc3a26de08f4fdfa9ea5b312c9a5c0ee2f
Paulo Amaral <paulo.amaral@gmail.com>
2017-07-03 02:05:45 +0100

    refactoring

--- application/lib/Components.php
+++ application/lib/Components.php
@@ -1,16 +1,18 @@
 <?php
 
-class Components {
-    
-    public static function code($code, $language) {
-        echo '<pre><code class="language-'.$language.' line-numbers">';
+class Components
+{
+
+    public static function code($code, $language)
+    {
+        echo '<pre><code class="language-' . $language . ' line-numbers">';
         echo trim($code);
         echo '</code></pre>';
     }
-    
-    public static function disqus($context) {
-        
-        $site_url = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != "off") ? "https://".$_SERVER['HTTP_HOST'] : "http://".$_SERVER['HTTP_HOST'];
+
+    public static function disqus($context)
+    {
+        $site_url = (! empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != "off") ? "https://" . $_SERVER['HTTP_HOST'] : "http://" . $_SERVER['HTTP_HOST'];
         $uri = explode("#", $_SERVER['REQUEST_URI']);
         $uri = $uri[0];
         $uri = explode("?", $uri);
@@ -18,20 +20,20 @@ class Components {
         
         ?>
         
-<div id="disqus_thread"></div>
-<script>
-
-var disqus_url = '<?php echo $site_url.$uri; ?>';
-
-(function() { // DON'T EDIT BELOW THIS LINE
-var d = document, s = d.createElement('script');
-s.src = 'https://paulodevtest.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
-</script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-
-<?php
+            <div id="disqus_thread"></div>
+            <script>
+            
+            var disqus_url = '<?php echo $site_url.$uri; ?>';
+            
+            (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://paulodevtest.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+            })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        
+        <?php
     }
 }
