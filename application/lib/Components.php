d41252ac9b447dc1475fc7410cf9d275ed65ce2e
Paulo Amaral <paulo.amaral@gmail.com>
2018-02-10 16:51:30 +0100

    Request::pageUrlForSeo() for discuss

--- application/lib/Components.php
+++ application/lib/Components.php
@@ -11,19 +11,13 @@ class Components
     }
 
     public static function disqus($context)
-    {
-        $site_url = (! empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != "off") ? "https://" . $_SERVER['HTTP_HOST'] : "http://" . $_SERVER['HTTP_HOST'];
-        $uri = explode("#", $_SERVER['REQUEST_URI']);
-        $uri = $uri[0];
-        $uri = explode("?", $uri);
-        $uri = $uri[0];
-        
+    {        
         ?>
         
             <div id="disqus_thread"></div>
             <script>
             
-            var disqus_url = '<?php echo $site_url.$uri; ?>';
+            var disqus_url = '<?php echo Request::pageUrlForSeo(); ?>';
             
             (function() { // DON'T EDIT BELOW THIS LINE
             var d = document, s = d.createElement('script');
